76f44d8d38948de67accaaa49fce33f7adb30f2c
hardikcoretus <78350989+hardikcoretus@users.noreply.github.com>
2021-02-01 17:18:01 +0530

    variable added from github web

--- app/src/main/java/com/coretus/secondgitdemo/MainActivity.java
+++ app/src/main/java/com/coretus/secondgitdemo/MainActivity.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    
+    String my_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        my_name="hardik";
+        Log.e("my_name",my_name);
+           
     }
 }
